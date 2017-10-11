612ebef8a2b64439b539b100541dc226e6a64154
Sasha Shundrikova <tim.bread@yandex.ru>
2017-10-12 00:58:17 +0300

    added LSystems
    without scaling

---
+++ LSystems/LSystems/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace LSystems
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.generate = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(664, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.openToolStripMenuItem.Text = "Open...";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.White;
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(462, 122);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 355);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "L-System Description";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(6, 19);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(188, 330);
+            this.textBox1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.White;
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Location = new System.Drawing.Point(6, 483);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(656, 125);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "L-System Commands";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.Location = new System.Drawing.Point(6, 19);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(644, 100);
+            this.textBox2.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(450, 450);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(482, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Number of iterations:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(592, 55);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(43, 20);
+            this.numericUpDown1.TabIndex = 5;
+            // 
+            // generate
+            // 
+            this.generate.Enabled = false;
+            this.generate.Location = new System.Drawing.Point(528, 82);
+            this.generate.Name = "generate";
+            this.generate.Size = new System.Drawing.Size(75, 23);
+            this.generate.TabIndex = 6;
+            this.generate.Text = "Generate";
+            this.generate.UseVisualStyleBackColor = true;
+            this.generate.Click += new System.EventHandler(this.generate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(664, 611);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.generate);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "L-System";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button generate;
+    }
+}
+
---
+++ LSystems/LSystems/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace LSystems
+{
+    public partial class Form1 : Form
+    {
+        //Dictionary<char, string> rules;
+        List<Tuple<char, string>> rules;
+        string axiom;
+        int angle;
+        string direction;
+        int iterations;
+        private string file;
+        string[] parameters;
+        //string path;
+        Stack<Tuple<double, double, double, double>> cache;
+        double startx = 10.0, starty = 10.0;
+        double scale;
+        Graphics g;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //rules = new Dictionary<char, string>();
+            rules = new List<Tuple<char, string>>();
+
+            Image bi = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bi);
+            g.Clear(Color.White);
+            //g.Dispose();
+            if (pictureBox1.Image != null)
+                pictureBox1.Image.Dispose();
+            pictureBox1.Image = bi;
+
+            cache = new Stack<Tuple<double, double, double, double>>();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                file = openFileDialog1.FileName;
+
+                if (!File.Exists(file))
+                {
+                    FileStream fs = File.Create(file);
+                    fs.Close();
+                }
+
+                StreamReader sr = new StreamReader(file.ToString());
+                textBox1.Text = sr.ReadToEnd();
+                sr.Close();
+                //Text += ' ' + System.IO.Path.GetFileName(file);
+                openFileDialog1.FileName = System.IO.Path.GetFileName(file);
+                generate.Enabled = true;
+                rules.Clear();
+                parser();
+            }
+        }
+
+        private void parser()
+        {
+            StreamReader sr = new StreamReader(file.ToString());
+            string line = sr.ReadLine();
+            parameters = line.Split(' ');
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] rule = line.Split('>');
+                //rules.Add(Convert.ToChar(rule[0]), rule[1]);
+                rules.Add(new Tuple<char, string>(Convert.ToChar(rule[0]), rule[1]));
+            }
+        }
+
+        private void makePath()
+        {
+            string current = axiom;
+            string next = axiom;
+            bool found;
+            int depth = 0;
+            while (depth < iterations)
+            {
+                current = next;
+                next = "";
+                for(int i = 0; i < current.Length; ++i)
+                {
+                    found = false;
+                    for (int j = 0; j < rules.Count; ++j)
+                    {
+                        if(current[i] == rules[j].Item1)
+                        {
+                            next = next + rules[j].Item2;
+                            found = true;
+                        }
+                    }
+                    if (!found) next = next + current[i];
+                }
+                depth++;
+            }
+            textBox2.Text = next;
+            drawLSystem(next);
+        }
+
+        private void drawLSystem(string ls)
+        {
+            double x = 0, y = 0, dx = 10, dy = 10;
+            switch (direction)
+            {
+                case "Up":
+                    x = pictureBox1.Width / 2;
+                    y = pictureBox1.Height;
+                    dx = 0;
+                    //scale / Math.Pow(2, iterations)
+                    dy = -(pictureBox1.Height/ Math.Pow(2, iterations + 2));
+                    break;
+                case "Down":
+                    x = pictureBox1.Width / 2;
+                    y = 0;
+                    dx = 0;
+                    dy = (pictureBox1.Height / Math.Pow(2, iterations + 2));
+                    break;
+                case "Left":
+                    x = pictureBox1.Width;
+                    y = pictureBox1.Height / 2;
+                    dx = -(pictureBox1.Width / Math.Pow(2, iterations + 2));
+                    dy = 0;
+                    break;
+                case "Right":
+                    x = 0;
+                    y = pictureBox1.Height / 2;
+                    dx = (pictureBox1.Width / Math.Pow(2, iterations + 2));
+                    dy = 0;
+                    break;
+            }
+            double rx, ry;
+            Pen pen = new Pen(Color.Black);
+            for (int i = 0; i < ls.Length; i++)
+            {
+                switch (ls[i])
+                {
+                    case 'F':
+                        g.DrawLine(pen, (float)x, (float)y, (float)(x + dx), (float)(y + dy));
+                        x += dx; y += dy;
+                        break;
+                    case '+':
+                        rx = dx; ry = dy;
+                        dx = rx * Math.Cos(Math.PI * angle / 180.0) - ry * Math.Sin(Math.PI * angle / 180.0);
+                        dy = rx * Math.Sin(Math.PI * angle / 180.0) + ry * Math.Cos(Math.PI * angle / 180.0);
+                        break;
+                    case '-':
+                        rx = dx; ry = dy;
+                        dx = rx * Math.Cos(-Math.PI * angle / 180.0) - ry * Math.Sin(-Math.PI * angle / 180.0);
+                        dy = rx * Math.Sin(-Math.PI * angle / 180.0) + ry * Math.Cos(-Math.PI * angle / 180.0);
+                        break;
+                    case '[':
+                        cache.Push(new Tuple<double, double, double, double>(x, y, dx, dy));
+                        break;
+                    case ']':
+                        Tuple<double, double, double, double> pos = cache.Peek();
+                        cache.Pop();
+                        x = pos.Item1; y = pos.Item2; dx = pos.Item3; dy = pos.Item4;
+                        break;
+                    default: break;
+                }
+            }
+        }
+
+        private void generate_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            iterations = (int)numericUpDown1.Value;
+            axiom = parameters[0];
+            angle = Convert.ToInt32(parameters[1]);
+            direction = parameters[2];
+            makePath();
+            pictureBox1.Refresh();
+        }
+    }
+}
