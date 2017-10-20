a0e11d29f60970f2f8cff33f298c32463343b672
anako18 <nuka97@yandex.ru>
2017-10-20 05:12:11 +0300

    Bezier implemented

---
+++ Bezier/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Bezier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(585, 570);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.button1.Location = new System.Drawing.Point(614, 512);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Draw Bezier";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(614, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 324);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.button2.Location = new System.Drawing.Point(614, 360);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Delete point";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.button3.Location = new System.Drawing.Point(614, 404);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Move point";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.button4.Location = new System.Drawing.Point(614, 559);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(120, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Clear";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(620, 492);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Add more points!";
+            this.label1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 594);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Bezier/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace Bezier
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+
+        Font drawFont = new Font("Arial", 10);
+        SolidBrush drawBrush = new SolidBrush(System.Drawing.Color.Black);
+        Pen pen = new Pen(Color.Black);
+
+        bool moving = false;
+
+       //List<Point> points;
+
+        Dictionary<char, Point> points;
+
+        char c = 'A';
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bmp;
+            g = Graphics.FromImage(bmp);
+            pen.StartCap = pen.EndCap = LineCap.Round;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            points = new Dictionary<char, Point>();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            label1.Visible = false;
+            if (moving)
+            {
+                points[(char)listBox1.SelectedItem] = new Point(e.X, e.Y);
+                redraw();
+                moving = false;
+            }
+            else
+            {
+                g.DrawString(c.ToString(), drawFont, drawBrush, e.X, e.Y);
+                g.DrawEllipse(pen, new Rectangle(e.X, e.Y, 1, 1));
+                points.Add(c, new Point(e.X, e.Y));
+                listBox1.Items.Add(c);
+                c++;
+                pictureBox1.Refresh();
+            }
+        }
+
+        private void my_drawbeziers()
+        {
+            Point prev = new Point(-100, -100);
+            for (int i = 3; i < points.Count; i += 3)
+            {
+                for (double t = 0; t <= 1; t += 0.0005)
+                {
+                    double c0 = (1 - t) * (1 - t) * (1 - t);
+                    double c1 = (1 - t) * (1 - t) * 3 * t;
+                    double c2 = (1 - t) * t * 3 * t;
+                    double c3 = t * t * t;
+                    double x = c0 * points.Values.ElementAt(i - 3).X + c1 * points.Values.ElementAt(i - 2).X + c2 * points.Values.ElementAt(i-1).X + c3 * points.Values.ElementAt(i).X;
+                    double y = c0 * points.Values.ElementAt(i - 3).Y + c1 * points.Values.ElementAt(i - 2).Y + c2 * points.Values.ElementAt(i - 1).Y + c3 * points.Values.ElementAt(i).Y;
+                    g.DrawEllipse(pen, new Rectangle((int)x, (int)y, 1, 1));
+                    if (prev.X != -100)
+                        g.DrawLine(pen, new Point((int)x, (int)y), prev);
+                    prev.X = (int)x;
+                    prev.Y = (int)y;
+                }
+                pictureBox1.Refresh();
+            }
+        }
+
+        //draw bezier
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (points.Count < 4)
+            {
+                label1.Visible = true;
+                return;
+            }
+
+            if ((points.Count - 1) % 3 == 0)
+            {
+                my_drawbeziers();
+                //pen.Color = Color.Red;
+                //g.DrawBeziers(pen, points.Values.ToArray());
+            }
+            else
+                label1.Visible = true;
+            pictureBox1.Refresh();
+        }
+
+        //delete point
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItems.Count == 0)
+                return;
+            points.Remove((char)listBox1.SelectedItem);
+            listBox1.Items.Remove(listBox1.SelectedItem);
+            redraw();
+           // listBox1.SelectedItem
+        }
+
+        //redraw
+        private void redraw()
+        {
+            g.Clear(System.Drawing.Color.White);
+
+            if (!(points == null))
+            for (int i = 0; i<points.Count; i++)
+            {
+                g.DrawEllipse(pen, new Rectangle(points.Values.ElementAt(i).X, points.Values.ElementAt(i).Y, 1, 1));
+                g.DrawString(points.Keys.ElementAt(i).ToString(), drawFont, drawBrush, points.Values.ElementAt(i).X, points.Values.ElementAt(i).Y);
+            }
+            pictureBox1.Refresh();
+        }
+
+        //move
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItems.Count == 0)
+                return;
+            moving = true;
+        }
+
+        //clear
+        private void button4_Click(object sender, EventArgs e)
+        {
+            points.Clear();
+            listBox1.Items.Clear();
+            g.Clear(System.Drawing.Color.White);
+            c = 'A';
+            pictureBox1.Refresh();
+        }
+
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bmp;
+            g = Graphics.FromImage(bmp);
+            redraw();
+        }
+    }
+}
